0cf3c4d0720a53ab17243ad4d766e6ff5b343427
Mario Nageh <marionageh7@gmail.com>
2023-03-20 13:46:01 -0700

    Printing Data types Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* Description: main - print to stdout data-types Sizes
+* Return: 0 if Success
+*/
+int main(void)
+{
+printf("Size Of a char: %d byte(s)\n", sizeof(char));
+printf("Size Of an int: %d byte(s)\n", sizeof(int));
+printf("Size Of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size Of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size Of a float: %d byte(s)\n", sizeof(float));
+return (0);
+}
