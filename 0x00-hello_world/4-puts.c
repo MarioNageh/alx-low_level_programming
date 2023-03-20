f308d244e5c4d40cff3a1993097f3f87e24b316a
Mario Nageh <marionageh7@gmail.com>
2023-03-20 13:21:47 -0700

    Adding Some Comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/*
+	This Function Created By Mario Nageh
+	To Print Using puts which adding EOF Automatic
+
+
+
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
