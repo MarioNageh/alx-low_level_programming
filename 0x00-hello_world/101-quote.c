83cb0b81f24fed28ea28c989545aa5727a624a26
Mario Nageh <marionageh7@gmail.com>
2023-03-20 13:58:59 -0700

    Printing To stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* Description: main printing To Standrand Error
+* Return: 1 To Indicated That is Error
+*/
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
