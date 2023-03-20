ba505e3bad8e60415828e81c78d7a1470e4aa423
Mario Nageh <marionageh7@gmail.com>
2023-03-20 13:28:04 -0700

    Print To stdout using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* Description: main - print to stdout using printf -> for formatting
+* Return: 0 if success
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
