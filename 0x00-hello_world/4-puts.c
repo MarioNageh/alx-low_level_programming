368c8470cf904cfabe882fcddc8a52389cd8e720
Mario Nageh <marionageh7@gmail.com>
2023-03-20 13:24:04 -0700

    Fix Doc Style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-/*
-	This Function Created By Mario Nageh
-	To Print Using puts which adding EOF Automatic
-
-
-
+/**
+* Description: main - print string to stdout
+* Return: 0 if success
+* This Function Created By Mario Nageh
+* To Print Using puts which adding EOF Automatic
 */
 int main(void)
 {
