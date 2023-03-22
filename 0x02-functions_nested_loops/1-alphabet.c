9c457f32e2dacaf37273e65f9105f1d24e38f875
Mario Nageh <marionageh7@gmail.com>
2023-03-22 10:07:06 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* Description: print_alphabet - to print lowercase alphabets
+* Return: return - void
+*/
+
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+
+}
