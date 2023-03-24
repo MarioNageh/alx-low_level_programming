2b0ae616f58a6107aa722160f8121877df808b77
Mario Nageh <marionageh7@gmail.com>
2023-03-24 06:38:22 -0700

    Fixing

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
-* Decription: main - To Test Positive or negative
+* main - To Test If Number Is + , -
 * Return: 0
 */
 
 int main(void)
 {
-int i;
+int i = 0;
 
-i = 0;
 positive_or_negative(i);
 return (0);
 }
