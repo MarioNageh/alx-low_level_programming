b817100bf03fbab09d10dc325ad5e4dfa1c9af40
Mario Nageh <marionageh7@gmail.com>
2023-03-24 19:19:49 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,12 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		
+		if (i == 100)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz ");
 		}
