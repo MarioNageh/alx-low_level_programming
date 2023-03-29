b6a146a23e78ae6e26daff1efc318490d3631471
Mario Nageh <marionageh7@gmail.com>
2023-03-29 03:08:45 -0700

    Print Reversed String

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_rev - print the string in reverse order
+* @s: string to reverse
+* Return: void
+*/
+
+void print_rev(char *s)
+{
+int i;
+
+for (i = strlen(s); i >= 0 ; i--)
+{
+	_putchar(*(s + i - 1));
+}
+
+}
