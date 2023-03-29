d3aca6d4ba856c6586731a0b26da914518c8abca
Mario Nageh <marionageh7@gmail.com>
2023-03-28 17:17:24 -0700

    String Length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "string.h"
+
+/**
+* Description: _strlen - To Get String Length
+* @s: the string s
+* Return: int size of string
+*/
+
+int _strlen(char *s)
+{
+return (strlen(s));
+
+}
