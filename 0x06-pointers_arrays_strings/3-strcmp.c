cddaed21bded8fdf0c78127b74d7f762aa8f438e
Mario Nageh <marionageh7@gmail.com>
2023-03-30 03:38:15 -0700

    String Compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -20,8 +20,8 @@ int _strcmp(char *s1, char *s2)
 	if (s1[i] == '\0' && s2[i] == '\0')
 		return (0);
 	else if (s1[i] > s2[i])
-		return (1);
+		return (s1[i] - s2[i]);
 	else
-		return (-1);
+		return (-s2[i] + s1[i]);
 }
 
