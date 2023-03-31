17a61aba14f3953a2f5cfbb044059667ded4f3a6
Mario Nageh <marionageh7@gmail.com>
2023-03-31 06:32:43 -0700

    To Upper

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* Description: cap-string - Capitalize The String
+* @s: string 
+* Return: char * to string
+*/
+
+char *cap_string(char *s)
+{
+	int i, j;
+	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			if (i == 0)
+			{
+				*(s + i) = *(s + i) - 32;
+			}
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+					{
+						*(s + i) = *(s + i) - 32;
+					}
+				}
+			}
+		}
+	i++;
+	}
+	return (s);
+}
