c58e80859347101c7867e443947377e20fcdca07
Mario Nageh <marionageh7@gmail.com>
2023-04-05 17:43:33 -0700

    String Compqre

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* Description: wildcmp - this function compare two strings
+* @s1: pointer to first string
+* @s2: pointer to second string
+* Return: 1 , 0
+*/
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
+	{
+		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+			return (0);
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+			return (1);
+	}
+
+	return (0);
+}
