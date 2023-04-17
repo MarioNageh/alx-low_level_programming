4224f016e7fb9312d01189f814410666c1bdfcab
Mario Nageh <marionageh7@gmail.com>
2023-04-17 07:58:48 -0700

    What's my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* description: main - Program That Prints It's Name
+* @argc: The Args Count
+* @argv: The Array Or Args
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
