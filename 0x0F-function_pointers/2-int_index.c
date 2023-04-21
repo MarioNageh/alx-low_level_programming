b2e081f364f7d6789c9142f7c0c165e06db51bd7
Mario Nageh <marionageh7@gmail.com>
2023-04-21 05:36:28 -0700

    int index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+* int_index - search in array using function
+* @a: array
+* @s: size of array
+* @f: function to serch
+* Return: index of element , 0 not found
+*/
+
+int int_index(int *a, int s, int(*f)(int))
+{
+	int k = s - 1;
+
+	if (f == NULL || a == NULL || s == 0)
+		return (0);
+
+
+	while (s--)
+	{
+		if (f(a[k - s]))
+			return (k - s);
+
+	}
+	return (0);
+}
