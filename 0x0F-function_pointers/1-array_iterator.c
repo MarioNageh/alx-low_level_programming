3c6df1090f3ee540fbbe53d2feb329690850d9af
Mario Nageh <marionageh7@gmail.com>
2023-04-21 05:23:45 -0700

    function to array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+* array_iterator - this function execute function on given array
+* @a: array pointer
+* @s: size
+* @f: function
+* Return: void
+*/
+
+void array_iterator(int *a, size_t s, void(*f)(int))
+{
+	size_t y = s - 1;
+
+	if (s == 0)
+		return;
+
+	if (f == NULL)
+		return;
+
+	while (s--)
+	{
+		f(a[y - s]);
+
+	}
+}
