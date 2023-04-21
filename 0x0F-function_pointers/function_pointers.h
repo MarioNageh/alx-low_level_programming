861d6c85e2dec6ca9aabf3b7c1bea9ae2a13c802
Mario Nageh <marionageh7@gmail.com>
2023-04-21 05:05:49 -0700

    Fixing

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
-#ifndef HEADER_FILE_NAME_H
-#define HEADER_FILE_NAME_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 #include <stdio.h>
 
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 
 
