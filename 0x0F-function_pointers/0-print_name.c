a65b1e45d8faa6c621ca30a12861cee8f8cfa324
Mario Nageh <marionageh7@gmail.com>
2023-04-21 04:52:27 -0700

    Function Pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+* print_name - function that take name\
+  and function and pass the name to function
+* @name: the param to print
+* @f: function to print
+* Return: void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_FILE_NAME_H
+#define HEADER_FILE_NAME_H
+#include <stdio.h>
+
+void print_name(char *name, void (*f)(char *));
+
+
+#endif 
