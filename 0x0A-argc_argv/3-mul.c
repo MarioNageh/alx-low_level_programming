c778915c540ce590171168ec6659c1c05a7415b1
Mario Nageh <marionageh7@gmail.com>
2023-04-26 03:55:23 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* description: main - Program That Prints It's Name
+* @argc: The Args Count
+* @argv: The Array Or Args
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+		return (-1);
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
