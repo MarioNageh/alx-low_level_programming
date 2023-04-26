5175545223cd860159fa8a59d83542b612159237
Mario Nageh <marionageh7@gmail.com>
2023-04-26 01:31:52 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - this function used to fill array s to n with b
+ * @s: array to fill
+ * @b: the byte to fill with
+ * @n: the size to fill
+ * Return: pointer to array
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
