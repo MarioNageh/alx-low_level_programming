1f4de601f6e9576883a93b694130f63bcfa634e0
Mario Nageh <marionageh7@gmail.com>
2023-04-26 01:57:39 -0700

    strchar

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,14 +8,17 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	i = 0;
-	while (s[i] != '\0')
+	if (*s == '\0')
+		return (s);
+	while (*s)
 	{
-		if (s[i] == c)
-			return (s + i);
-		i++;
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
 	}
-	return NULL;
+	if (c == '\0')
+		return (s);
+	return (NULL);
 }
