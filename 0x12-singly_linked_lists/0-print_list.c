199f817f52a96cf71c3377c4f15cc68621cabaac
Mario Nageh <marionageh7@gmail.com>
2023-04-27 04:22:19 -0700

    travese linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all linked list elemnts
+ * @h: pointer to linked list
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int n = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		if (h->str == NULL || h->str == '\0')
+		{
+			puts("[0] (nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		n++;
+		h = h->next;
+	}
+
+	return (n);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef LINKEDLIST_LISTS_H
+#define LINKEDLIST_LISTS_H
+
+#include <unistd.h>
+
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+
+#endif //LINKEDLIST_LISTS_H
