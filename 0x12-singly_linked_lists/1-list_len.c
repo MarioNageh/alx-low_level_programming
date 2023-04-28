5d6d46622ff2be92726f9b8608a69854bf31c277
Mario Nageh <marionageh7@gmail.com>
2023-04-27 18:34:23 -0700

    count elemnts

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - return the number of elements
+ * @h: pointer to linked list
+
+ * Return: number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	int n = 0;
+
+	while (h) {
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
