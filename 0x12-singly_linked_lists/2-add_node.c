e5e3bdada73ed5fa93a0a2a46655eb6bd0a3659e
Mario Nageh <marionageh7@gmail.com>
2023-04-27 18:56:58 -0700

    Solving

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,19 +17,18 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (str == NULL)
 	{
-		free(new_element);
 		return (NULL);
 	}
 
 
+	for (len_of_data = 0; str[len_of_data];len_of_data++)
+		;
 
-	for (len_of_data = 0; str[len_of_data];)
-		len_of_data++;
 
 	new_element->str = strdup(str);
 	new_element->len = len_of_data;
 	new_element->next = *head;
 
 	*head = new_element;
-	return (new_element);
+	return (*new_element);
 }
