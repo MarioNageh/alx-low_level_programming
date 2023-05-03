c9c4063ccc5f24e0febf7629e475347ba4d2f57e
Mario Nageh <marionageh7@gmail.com>
2023-05-02 18:27:20 -0700

    Linked List Task

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+
+/**
+* pop_listint - print linked list
+*@h: pointer of head of linked list
+*Return: Void
+*/
+
+int  pop_listint(listint_t **h)
+{
+	listint_t *head;
+	int data;
+
+	if (h == NULL)
+		return (0);
+
+	head = *h;
+	data = head->n;
+
+	*h = (*h)->next;
+
+	free(head);
+	return (data);
+}
