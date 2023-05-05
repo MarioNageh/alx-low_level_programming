147ce4e2a6efdb0a67d72b49993ab3214cca227c
Mario Nageh <marionageh7@gmail.com>
2023-05-05 09:00:48 -0700

    other impelementaion

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,15 +12,15 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 
 	unsigned int flips_number = 0;
-	unsigned int mask = 1, max_flips = (sizeof(unsigned long int) * 8) - 1;
+	unsigned int mask = 1;
 
-	while (max_flips)
+	while (n != 0 || m != 0)
 	{
 		if ((mask & n) != (mask & m))
 			flips_number += 1;
 
-		mask = mask << 1;
-		max_flips -= 1;
+		n = n >> 1;
+		m = m >> 1;
 	}
 
 	return (flips_number);
