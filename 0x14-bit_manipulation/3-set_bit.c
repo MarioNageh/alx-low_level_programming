4aba46ba2c3ceec535039e6f20874608528a0cfd
Mario Nageh <marionageh7@gmail.com>
2023-05-04 19:49:26 -0700

    set bit using or |

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * set_bit - return the bit at index
+ * @n: the number
+ * @index: index value
+ *
+ * Return: bit value at this index
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+
+	unsigned long int mask, value;
+
+	/* if index bigger thant bit 0 , 63 */
+	if (index > ((sizeof(unsigned long int) * 8) - 1))
+		return (-1);
+
+	mask = 1 << index;
+	*n = *n | mask;
+
+	return (1);
+
+}
