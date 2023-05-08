324e47d509e476233203015ca28c9aeed38e07f0
Mario Nageh <marionageh7@gmail.com>
2023-05-08 06:30:12 -0700

    Close FD

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -36,10 +36,8 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[i] != '\0')
 		i++;
 
-	status = write(fd, text_content, i);
+	write(fd, text_content, i);
 
-	if (!status)
-		return (-1);
 
 
 	close(fd);
