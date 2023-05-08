45828cd37ba3790fec2aa30394c4efa0d652365e
Mario Nageh <marionageh7@gmail.com>
2023-05-08 07:36:11 -0700

    Copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,23 +11,16 @@
  */
 int main(int ac, char **av)
 {
-
 	int fd_to_write, fd_to_read;
 	int i = 0, buffer_read = BUFFER_SIZE;
 	char buf[BUFFER_SIZE];
 
 	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
 	fd_to_read = open(av[1], O_RDONLY);
 	if (fd_to_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
 	fd_to_write = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
 														  | S_IRGRP | S_IWGRP | S_IROTH);
@@ -37,28 +30,20 @@ int main(int ac, char **av)
 		close(fd_to_write), exit(99);
 	}
 
+
 	while (buffer_read == BUFFER_SIZE)
 	{
 		buffer_read = read(fd_to_read, buf, BUFFER_SIZE);
 		if (buffer_read == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-			exit(98);
-		}
-
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 		i = write(fd_to_write, buf, buffer_read);
-
 		if (i < buffer_read)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
-
-
 	if (close(fd_to_read) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to_read), exit(100);
 
 	if (close(fd_to_write) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to_write), exit(100);
-
-
 	return (0);
 }
