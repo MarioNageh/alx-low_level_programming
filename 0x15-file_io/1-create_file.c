57fa2196ce89ab3a69cb3f1ed2d449a059d376b9
Mario Nageh <marionageh7@gmail.com>
2023-05-08 06:23:07 -0700

    create file with data

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+/* Created by mario on 5/8/2023. */
+#include "main.h"
+
+/**
+ * create_file - create file and write content to it
+ * @filename: file name
+ * @text_content: content to write
+ * Return: 1 if success 0 if fail
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, mode, flag, i = 0, status = 0;
+
+	if (!filename)
+		return (0);
+
+
+	/* rw- --- --- */
+	/* 8 << 1  | 7 << 1 */
+	/* S_IRUSR | S_IWUSR this 0x180 0600 octal */
+
+	mode = S_IRUSR | S_IWUSR;
+	flag = O_WRONLY | O_CREAT | O_TRUNC;
+	fd = open(filename, flag, mode);
+
+
+	/* Failed To Open File or get File Descriptor */
+	if (fd == -1)
+		return (-1);
+
+
+	if (!text_content)
+		text_content = "";
+
+	while (text_content[i] != '\0')
+		i++;
+
+	status = write(fd, text_content, i);
+
+	if (!status)
+		return (0);
+
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+
 
 #endif
